057f369829c8081aac4d91a9c8a04fdde009e549
RobertYE <wu87988622@gmal.com>
2018-05-17 13:00:20 +0800

    update selenium chrome driver on heroku add pip chromedriver_installer

--- crawler/parser.py
+++ crawler/parser.py
@@ -14,7 +14,7 @@ def parser_post(request):
         if url is not '' and html is '':
             #driver = webdriver.Chrome(
             #    executable_path=r'/Users/qq/PycharmProjects/parseltongue/chromedriver/chromedriver')
-            driver = webdriver.Chrome(executable_path=r"/app/.apt/usr/bin/google-chrome")
+            driver = webdriver.Chrome(executable_path=r'/app/.apt/usr/bin/google-chrome')
             driver.get(url)
             html = driver.page_source
             driver.close()
