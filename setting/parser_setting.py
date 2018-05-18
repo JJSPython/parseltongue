67b99a09634900f0aa3898cab2f821844622217f
RobertYE <wu87988622@gmal.com>
2018-05-18 17:44:13 +0800

    update selenium chrome driver on heroku

--- setting/parser_setting.py
+++ setting/parser_setting.py
@@ -22,7 +22,8 @@ def setting_post(request):
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--headless')
-            driver = webdriver.Chrome(executable_path='/app/.apt/usr/bin/google-chrome', chrome_options=chrome_options)
+            chrome_options.add_argument('--remote-debugging-port=9222')
+            driver = webdriver.Chrome(chrome_options=chrome_options)
             driver.get(url)
             html = driver.page_source
             driver.close()
